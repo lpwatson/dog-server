cb812b48a347b6a753b3bdc99289ce2919047637
Liam Watson <liamwatson@liam.local>
2023-02-17 12:30:52 +0000

    adds env

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(3001);
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT);
 }
 bootstrap();
